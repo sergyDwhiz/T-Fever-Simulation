d900fc4d20420681b732048cf91b7b3e05feec6a
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2024-02-06 21:35:29 +0100

    Add a title to the plot

--- src/simulate_diff.py
+++ src/simulate_diff.py
@@ -36,12 +36,14 @@ y0 = S0, I0, R0
 ret = odeint(deriv, y0, t, args=(N, beta, gamma))
 S, I, R = ret.T
 
+ax.set_title('SIR Model Simulation') # Add a title to the plot
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
 ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
 ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+
 ax.set_xlabel('Time /days')
 ax.set_ylabel('Number (1000s)')
 ax.set_ylim(0,1.2)
